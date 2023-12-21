3b58b65cd27659e10038058b806d37f3e148a820
Oleg Zavorotniy <o.zavorotniy@hotmail.com>
2023-12-22 00:37:56 +0300

    Lesson1

---
+++ Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    num = -num;
+}
+if (num >= 100 && num <= 999)
+{
+    int numFirst = num / 100;
+    int numSecond = num % 10;
+    int result = numFirst + numSecond;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
